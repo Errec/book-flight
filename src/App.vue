1fee88dd1a3b864cdbbdd24cdd7844edf56a2433
Raniro Coelho <errecme@gmail.com>
2018-12-28 22:50:58 -0200

    Add App style

--- src/App.vue
+++ src/App.vue
@@ -25,4 +25,33 @@ export default {
   @import 'styles/base/normalize'
   @import 'styles/base/reset'
   @import 'styles/variables/colors'
+
+  #app
+    width: 100%
+    height: 100%
+    position: relative
+    background-color: $MMbgColor
+    color: $MMGrey
+    overflow-y: scroll
+    overflow-x: hidden
+    &::-webkit-scrollbar
+      display: none
+    -ms-overflow-style: none
+    overflow: -moz-scrollbars-none
+
+  .app--padding-top
+    padding-top: 60px
+
+  @media screen and (min-width: 600px)
+    #app
+      box-shadow: 8px 9px 47px -8px rgba(0,0,0,0.75);
+      padding: 12px
+      max-width: 450px
+      position: absolute
+      height: auto
+      overflow-y: none
+      top: 24px
+      right: 0
+      left: 0
+      margin: 0 auto 24px auto
 </style>
