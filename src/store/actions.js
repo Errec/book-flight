563fe5e39aa9ab5d75611ccb1ed26f8f246c00cb
Raniro Coelho <errecme@gmail.com>
2019-01-02 20:16:21 -0200

    Update direction tab

--- src/store/actions.js
+++ src/store/actions.js
@@ -14,7 +14,7 @@ export default {
   addPassengerData ({commit}, payload) {
     commit('setPassengersData', payload)
   },
-  loadFlights ({commit}, payload) {
+  async loadFlights ({commit}, payload) {
     const SEARCH_FLIGHTS_API = 'https://flight-price-hmg.maxmilhas.com.br'
     const token = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJtYXhtaWxoYXMuY29tLmJyIiwiaWF0IjoxNTA5MTIwMTAxLCJleHAiOjE1MTA0MTYxMDEsImF1ZCI6InRlc3RlLWZyb250ZW5kIiwic3ViIjoidGVzdGUtZnJvbnRlbmQiLCJlbnYiOiJobWcifQ.nM6wMem6dxF0CcDlig5iA9az5ZfmbXDjq1e4ypZXwjU'
 
